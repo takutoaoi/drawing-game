d050e3ce4d3dbd7621ed20286b2d89861a05ee56
aoi takuto <a.013053@gmail.com>
2019-09-07 00:28:25 +0900

    react setup

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Grommet} from 'grommet';
+
+ReactDOM.render(
+    <Grommet plain>
+
+    </Grommet>,
+    document.getElementById('root')
+);
