bb599f67ea9d2a6e3636408d574b8f6966e39a9b
aoi takuto <a.013053@gmail.com>
2019-09-16 15:20:05 +0900

    読み込んでいないimport

--- src/Templates/StartPage.tsx
+++ src/Templates/StartPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Hero } from '../Organisms/Hero'
 import { QuestionMarkBackGround } from '../Molecules/QuestionMarkBackGround'
-import { HeroButtons } from '../utils/defenitions'
 
 export const StartPageTemplate = (props:{onClickButton:Function}) => {
   return (
